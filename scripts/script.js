253cf230364d83e5d5838d4db015023c762bf6d7
evilya2505 <like17seventeen@yandex.ru>
2021-04-13 20:56:58 +0300

    feat: сверстать header content footer

---
+++ scripts/script.js
@@ -0,0 +1,34 @@
+let btnEdit = document.querySelector('.profile__edit-btn');
+let popupWindow = document.querySelector('.popup');
+let btnClose = document.querySelector('.popup__close-btn');
+let profileName = document.querySelector('.profile__name');
+let profileOccupation = document.querySelector('.profile__occupation');
+let formElement = document.querySelector('.form');
+let inputName = document.querySelector('.form__name');
+let inputOccupation = document.querySelector('.form__occupation');
+
+function openPopup() {
+  inputOccupation.value = profileOccupation.textContent;
+  inputName.value = profileName.textContent;
+  popupWindow.classList.add('popup_opened');
+}
+
+function closePopup() {
+  popupWindow.classList.remove('popup_opened');
+}
+
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileName.textContent = inputName.value;
+  profileOccupation.textContent = inputOccupation.value;
+  closePopup();
+}
+
+btnClose.addEventListener('click', closePopup);
+
+btnEdit.addEventListener('click', openPopup);
+
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
